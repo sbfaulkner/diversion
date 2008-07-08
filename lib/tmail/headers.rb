97c596c7ba59d6e0bb462c8cf6a0f81d5e87b652
S. Brent Faulkner <brentf@unwwwired.net>
2008-07-08 17:04:58 -0400

    DRY it up baby

--- lib/tmail/headers.rb
+++ lib/tmail/headers.rb
@@ -1,81 +1,43 @@
+#
+# add support for custom x-diverted-... headers in order to preserve the original recipients
+#
 module TMail
+  DIVERTED_HEADERS=%w(to cc bcc)
+  X_DIVERTED_HEADERS=DIVERTED_HEADERS.collect { |h| "x-diverted-#{h}" }
+  
+  # specify that the x-diverted-... headers specify an email address
   class HeaderField
-    FNAME_TO_CLASS.merge!(
-      'x-diverted-to' => AddressHeader,
-      'x-diverted-cc' => AddressHeader,
-      'x-diverted-bcc' => AddressHeader
-    )
+    # indicate that the x-diverted-... headers are address headers
+    X_DIVERTED_HEADERS.each { |h| FNAME_TO_CLASS[h] => AddressHeader }
   end
   
   class Mail
-    ALLOW_MULTIPLE.merge!(
-      'x-diverted-to' => true,
-      'x-diverted-cc' => true,
-      'x-diverted-bcc' => true
-    )
-    
-    #
-    # diversion
-    #
+    # allow multiple values for the headers
+    X_DIVERTED_HEADERS.each { |h| ALLOW_MULTIPLE[h] => true }
 
-    def x_diverted_to_addrs( default = nil )
-      if h = @header['x-diverted-to']
-        h.addrs
-      else
-        default
-      end
-    end
-
-    def x_diverted_cc_addrs( default = nil )
-      if h = @header['x-diverted-cc']
-        h.addrs
-      else
-        default
-      end
-    end
-
-    def x_diverted_bcc_addrs( default = nil )
-      if h = @header['x-diverted-bcc']
-        h.addrs
-      else
-        default
-      end
-    end
-
-    def x_diverted_to_addrs=( arg )
-      set_addrfield 'x-diverted-to', arg
-    end
-
-    def x_diverted_cc_addrs=( arg )
-      set_addrfield 'x-diverted-cc', arg
-    end
-
-    def x_diverted_bcc_addrs=( arg )
-      set_addrfield 'x-diverted-bcc', arg
-    end
-
-    def x_diverted_to( default = nil )
-      addrs2specs(x_diverted_to_addrs(nil)) || default
-    end
-
-    def x_diverted_cc( default = nil )
-      addrs2specs(x_diverted_cc_addrs(nil)) || default
-    end
-
-    def x_diverted_bcc( default = nil )
-      addrs2specs(x_diverted_bcc_addrs(nil)) || default
-    end
-
-    def x_diverted_to=( *strs )
-      set_string_array_attr 'X-Diverted-To', strs
-    end
-
-    def x_diverted_cc=( *strs )
-      set_string_array_attr 'X-Diverted-Cc', strs
-    end
-
-    def x_diverted_bcc=( *strs )
-      set_string_array_attr 'X-Diverted-Bcc', strs
+    X_DIVERTED_HEADERS.each do |h|
+      h_name = h.underscore
+      self.instance_eval <<-METHODS
+        def #{h_name}_addrs(default = nil)
+          if h = @header['#{h}']
+            h.addrs
+          else
+            default
+          end
+        end
+        
+        def #{h_name}_addrs=(arg)
+          set_addrfield '#{h}', arg
+        end
+        
+        def #{h_name}(default = nil)
+          addrs2specs(#{h_name}_addrs(nil)) || default
+        end
+        
+        def #{h_name}=(*strs)
+          set_string_array_attr '#{h.gsub(/\b([a-z])/) { |m| m.capitalize}}', strs
+        end
+      METHODS
     end
   end
 end
